c1645b3371ab21ae1b946ea1f5175b814fda37e7
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-18 23:27:54 +0600

    server running & route defining

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+app.get('/', (rea, res) => {
+
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 })
