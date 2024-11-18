c9dc958c8f05e805453017901c0baa8b52cf8216
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-18 23:39:29 +0600

    post page create in another route

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
-app.get('/', (rea, res) => {
+app.get('/', (req, res) => {
   res.send('<div><h1>Hello World</h1><p>Hello Fleckians</p></div>')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('<h1>I am post page</h1>')
 })
 
 app.listen(PORT, () => {
