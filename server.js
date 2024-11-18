ced9df3269f0ce8452bbb3bd390b968751c09cab
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-18 23:37:01 +0600

    nodemon restart test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.get('/', (rea, res) => {
-  res.send('<h1>Hello World</h1>')
+  res.send('<div><h1>Hello World</h1><p>Hello Fleckians</p></div>')
 })
 
 app.listen(PORT, () => {
