eef93ae79a207fa476b518d2656b1840f3cf8022
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-19 11:24:19 +0600

    Route id end

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -20,14 +20,16 @@ router.get('/:id', (req, res, next) => {
     message: 'I am a single Contact'
   })
 })
+
 router.put('/:id', (req, res, next) => {  
   res.json({
-    message: 'I am a single Contact'
+    message: 'I am a PUT Route'
   })
 })
+
 router.delete('/:id', (req, res, next) => {  
   res.json({
-    message: 'I am a single Contact'
+    message: 'I am a Delete Route'
   })
 })
 
