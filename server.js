83f052c58b52eae563abbd2746abd26f9e0b1bab
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-19 08:35:06 +0600

    app.use command

--- server.js
+++ server.js
@@ -6,23 +6,12 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+app.use('/api/contacts', contactRoute)
+
 app.get('/', (req, res) => {
   res.send('<div><h1>Hello World</h1><p>Hello Fleckians</p></div>')
 })
 
-app.get('/posts', (req, res) => {
-  res.send('<h1>I am post page</h1>')
-})
-
-app.get('/api/contacts', (req, res) => {
-  res.json(contacts)
-})
-
-app.post('/api/contacts', (req, res) => {
-  res.json({
-    message: 'I am post method'
-  })
-})
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
