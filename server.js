f1ebbe5034b8926bff2da3850b26fcc432d3aa09
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-28 16:53:14 +0600

    successfully body-parser active

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const contactRoute = require('./api/routes/contact')
 const app = express()
 app.use(morgan('dev'))
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
 const PORT = process.env.PORT || 4000
 
-app.use((req, res, next) => {
-  console.log('I am a Middleware Function')
-  next()
-})
+
 
 app.use('/api/contacts', contactRoute)
 
