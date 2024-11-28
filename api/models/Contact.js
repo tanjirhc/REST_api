2f094885e9e22c03247fad5969422b16a0c96b3c
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-28 22:26:25 +0600

    validator install

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -1,2 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const ContactSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 3
+  },
+  phone: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    validate: {
+      validator: (v) => {
+
+      },
+      message: `${v} is not an email`
+    }
+  }
+})
