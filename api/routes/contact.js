c9c2017ef7cc91db3b49a0567a6e659c24912562
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-29 05:36:19 +0600

    importing controller contact

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -1,51 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const Contact = require('../models/Contact')
+
+const contactController = require('../controllers/contact')
 
 
 // Get
-router.get('/', (req, res, next) => {
-  Contact.find()
-    .then(contacts => {
-      res.status(200).json({
-        message: 'All Contacts',
-        contacts
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({
-        message: 'Error Occured',
-        error: err
-      })
-    })
-})
+router.get('/', contactController.getAllContactController)
 
 // POST
-router.post('/', (req, res, next) => {
-
-  const contact = new Contact({
-    name: req.body.name,
-    phone: req.body.phone,
-    email: req.body.email
-  })
-  
-  contact.save()
-   .then(data => {
-    res.status(201).json({
-      message: 'Contact Added',
-      contact: data
-    })
-   })
-   .catch(err => {
-    console.log(err)
-    res.status(500).json({
-      message: 'Error Occured',
-      error: err
-    })
-  })
-})
+router.post('/', contactController.postNewContactController)
 
 router.get('/:id', (req, res, next) => {  
   res.json({
