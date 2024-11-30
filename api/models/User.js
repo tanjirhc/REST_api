f7de902e108886d4a61dfb87f68be7df15999203
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-30 07:37:05 +0600

    creating Schema

---
+++ api/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const valid = require('validator')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    validate: {
+      validator: (v) => {
+        return validator.isEmail(v)
+      },
+      message: '${v} is not an email'
+    }
+  },
+  password: String
+})
