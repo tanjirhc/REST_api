77f68d22f838f4c4aadaac560ca9a333fd6b5aae
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-30 08:11:23 +0600

    login, register & get routers created

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/login', (req, res, next) => {
+
+})
+
+router.post('/register', (req, res, next) => {
+  
+})
+
+router.get('/users', (req, res, next) => {
+  
+})
