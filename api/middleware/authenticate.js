bdbc71fcebf66556e8670170a5ddd848267cd492
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-06 18:20:10 +0600

    bearer token created

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -4,9 +4,17 @@ const authenticate = (req, res, next) => {
 
   try {
 
+    const token = req.header.authorization.split(' ')[1]
+    const decode = jwt.verify(token, 'SECRET')
+
+    req.user = decode
+    next()
+
   } catch(error) {
     res.json({
       message: "Authentication Failed"
     })
   }
 }
+
+module.exports = authenticate
