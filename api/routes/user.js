f84209dc107db9a83f0bbcde0e528bea52c07d49
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-06 18:22:13 +0600

    authenticate imported in routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const authenticate = require('../middleware/authenticate')
 
 const userController = require('../controllers/user')
 
@@ -7,6 +8,6 @@ router.post('/login', userController.loginController)
 
 router.post('/register', userController.registerController)
 
-router.get('/', userController.getAllUser)
+router.get('/', authenticate, userController.getAllUser)
 
 module.exports = router
